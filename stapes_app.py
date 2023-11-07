f1a73aedfae11f07c4bf06974ac9d8f56d4de66e
obinwosu <117573177+obinwosu@users.noreply.github.com>
2023-11-06 21:53:20 -0500

    Update stapes_app.py
    put inputs in main body

--- stapes_app.py
+++ stapes_app.py
@@ -10,35 +10,34 @@ st.title('Predictive Model for Stapes Surgery Outcomes')
 st.write("a machine learning app for predicting postoperative airbone gap using only presurgical variables")
 st.markdown("<p style='font-size: 12px;'>This app is currently for research purposes only.</p>", unsafe_allow_html=True)
 
-
 # Input features
-st.sidebar.header("Enter your features")
+st.header("Enter your features")
 
-feature1 = st.sidebar.number_input('Surgeon Experience (years)', min_value=0, max_value=50, value=10, step=1)
-feature2 = st.sidebar.number_input('Preoperative Air Pure Tone Average', min_value=0, max_value=200, value=50, step=1)
-feature3 = st.sidebar.number_input('Preoperative Bone Pure Tone Average', min_value=0, max_value=200, value=30, step=1)
+feature1 = st.number_input('Surgeon Experience (years)', min_value=0, max_value=50, value=10, step=1)
+feature2 = st.number_input('Preoperative Air Pure Tone Average', min_value=0, max_value=200, value=50, step=1)
+feature3 = st.number_input('Preoperative Bone Pure Tone Average', min_value=0, max_value=200, value=30, step=1)
 
 # Calculate ABG_pre
 abg_pre = feature2 - feature3
 
 # Display the calculated ABG_pre
-#st.sidebar.write(f'Calculated ABG_pre: {abg_pre}')
+#st.write(f'Calculated ABG_pre: {abg_pre}')
 
-feature4 = st.sidebar.number_input('Preoperative Airbone Gap', min_value=0, max_value=100, value=abg_pre, step=1)
-feature5 = st.sidebar.number_input('Age', min_value=18, max_value=100, value=18, step=1)
+feature4 = st.number_input('Preoperative Airbone Gap', min_value=0, max_value=100, value=abg_pre, step=1)
+feature5 = st.number_input('Age', min_value=18, max_value=100, value=18, step=1)
 
 # Select 'Left' or 'Right' for laterality
-selected_laterality = st.sidebar.radio('Select Laterality', ['Left', 'Right'])
+selected_laterality = st.radio('Select Laterality', ['Left', 'Right'])
 laterality_left = np.uint8(1 if selected_laterality == 'Left' else 0)
 laterality_right = np.uint8(1 if selected_laterality == 'Right' else 0)
 
 # Create a radio button for race selection
-selected_race = st.sidebar.radio('Select Race', ['White', 'Black', 'NativeAmerican', 'Asian', 'Hawaiian', 'Hispanic', 'Other'])
+selected_race = st.radio('Select Race', ['White', 'Black', 'NativeAmerican', 'Asian', 'Hawaiian', 'Hispanic', 'Other'])
 race_options = ['White', 'Black', 'NativeAmerican', 'Asian', 'Hawaiian', 'Hispanic', 'Other']
 race_selection = [int(1 if selected_race == race else 0) for race in race_options]
 
 # Create a radio button for gender selection
-selected_gender = st.sidebar.radio('Select Gender', ['Male', 'Female'])
+selected_gender = st.radio('Select Gender', ['Male', 'Female'])
 gender_selection = [int(1 if selected_gender == 'Male' else 0), int(1 if selected_gender == 'Female' else 0)]
 
 # Combine all feature selections
