54672a478efa765d3b2aedb21416dedb91ee55e5
obinwosu <117573177+obinwosu@users.noreply.github.com>
2023-11-06 21:47:09 -0500

    Update stapes_app.py

--- stapes_app.py
+++ stapes_app.py
@@ -9,7 +9,7 @@ model = pickle.load(open('best_rf_model1.pkl', 'rb'))
 st.title('Predictive Model for Stapes Surgery Outcomes')
 st.write("a machine learning app for predicting postoperative airbone gap using only presurgical variables")
 st.write("This app is for research purposes only.")
-st.markdown("<p style='font-size: 16px;'>This app is for research purposes only.</p>", unsafe_allow_html=True)
+st.markdown("<p style='font-size: 10px;'>This app is for research purposes only.</p>", unsafe_allow_html=True)
 
 
 # Input features
